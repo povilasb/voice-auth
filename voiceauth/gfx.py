03467dca3834c98156a16a7299b559bcc8151168
Povilas Balciunas <balciunas90@gmail.com>
2017-04-09 12:48:45 +0300

    Add utility function to create spectrogram from audio stream

--- voiceauth/gfx.py
+++ voiceauth/gfx.py
@@ -1,9 +1,22 @@
 from typing import List
 
 import matplotlib.pyplot as plt
+from scipy import signal
+from skimage import io
+import numpy as np
 
 
 def plot_vector(v: List[int]) -> None:
     plt.plot(v)
     plt.ylabel('some numbers')
     plt.show()
+
+
+def make_spectrogram(stream: bytes) -> np.array:
+    f, t, Sxx = signal.spectrogram(stream, 44100)
+    plt.pcolormesh(t, f, Sxx)
+    plt.axis('off')
+
+    img_file = '/tmp/spectrogram.png'
+    plt.savefig(img_file)
+    return io.imread(img_file)
